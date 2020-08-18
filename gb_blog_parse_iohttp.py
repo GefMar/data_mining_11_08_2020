e84e8fadda0e31e6ed38db2f3fedbe93babf3feb
gefest <gefest@me.com>
2020-08-18 17:40:50 +0300

    les2 hw async

--- gb_blog_parse_iohttp.py
+++ gb_blog_parse_iohttp.py
@@ -1,4 +1,4 @@
-from typing import Dict
+import random
 import asyncio
 
 import re
@@ -14,37 +14,40 @@ class GbBlogParser:
 
     def __init__(self, mongo_collection):
         self.collection = mongo_collection
-        self.timeout = aiohttp.ClientTimeout(total=60.0)
+        self.timeout = aiohttp.ClientTimeout(total=120.0)
+
+    async def request(self, url) -> BeautifulSoup:
+        while True:
+            await asyncio.sleep(random.randint(1, 5) / random.randint(1, 3))
+            async with aiohttp.ClientSession(timeout=self.timeout) as session:
+                async with session.get(url) as response:
+                    if response.status == 200:
+                        soap = BeautifulSoup(await response.text(), 'lxml')
+                        break
+                    elif response.status >= 500:
+                        await asyncio.sleep(1.3)
+                        continue
+        return soap
 
     async def parse(self, url=start_url):
+        while url:
+            soap = await self.request(url)
+            url = self.get_next_page(soap)
+            await asyncio.wait([self.posts_parse(url) for url in self.search_post_links(soap)])
 
-        async with aiohttp.ClientSession(timeout=self.timeout) as session:
-            while url:
-                async with session.get(url) as response:
-                    soap = BeautifulSoup(await response.text(), 'lxml')
-                url = self.get_next_page(soap)
-                await asyncio.create_task(self.posts_parse(soap))
-
-    async def posts_parse(self, soap: BeautifulSoup):
-        async with aiohttp.ClientSession(timeout=self.timeout) as session:
-            links = self.search_post_links(soap)
-            tasks = []
-            async for url in links:
-                async with session.get(url) as response:
-                    soap = BeautifulSoup(await response.text(), 'lxml')
-                tasks.append(self.get_post_data(soap))
-            await asyncio.wait(tasks)
+    async def posts_parse(self, url):
+        soap = await self.request(url)
+        await self.get_post_data(soap)
 
     def get_next_page(self, soap: BeautifulSoup) -> str:
         ul = soap.find('ul', attrs={'class': 'gb__pagination'})
         a = ul.find('a', text=re.compile('›'))
         return f'{self.domain}{a.get("href")}' if a and a.get("href") else None
 
-    async def search_post_links(self, soap: BeautifulSoup):
+    def search_post_links(self, soap: BeautifulSoup):
         wrapper = soap.find('div', attrs={'class': "post-items-wrapper"})
         posts = wrapper.find_all('div', attrs={'class': 'post-item'})
-        for itm in {f'{self.domain}{itm.find("a").get("href")}' for itm in posts}:
-            yield itm
+        return {f'{self.domain}{itm.find("a").get("href")}' for itm in posts}
 
     # todo Извлечение данных из страницы материала
     async def get_post_data(self, soap: BeautifulSoup):
